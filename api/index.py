444d39555e431fbe17afb0df90c28a2c3b6ad65a
unknown <topone1212talent@gmail.com>
2023-10-28 05:58:57 +0300

    second

--- api/index.py
+++ api/index.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from freshbooks import Client
 
+freshBooksClient = Client(
+    client_id="71f8d9e091f36ac5e40b8939b4793da813c3df51b45735271bb380ede4e903d9",
+    client_secret="cc2bc7903a859510616d0582d60d4d8d6e86dffdc21838b9a39a20f1df559c10",
+    redirect_uri="https://nextjs-fastapi-starter-bay-seven.vercel.app/"
+)
 app = FastAPI()
-
+print(freshBooksClient)
 @app.get("/api/python")
 def hello_world():
-    return {"message": "Hello World"}
+    return {"message": "Hello World123"}
